e49720870f5cd757a4b9190f51f06974b4c5d523
49EHyeon42 <49ehyeon42@gmail.com>
2025-07-19 11:05:17 +0900

    fix: 코드 개선, afterHandshake 메서드 작성

--- src/main/kotlin/dev/ehyeon/hydrangea/common/handshakeinterceptor/AuthHandshakeInterceptor.kt
+++ src/main/kotlin/dev/ehyeon/hydrangea/common/handshakeinterceptor/AuthHandshakeInterceptor.kt
@@ -5,6 +5,7 @@ import dev.ehyeon.hydrangea.common.property.TokenProperty
 import dev.ehyeon.hydrangea.common.service.AuthService
 import dev.ehyeon.hydrangea.user.exception.UserNotFoundException
 import dev.ehyeon.hydrangea.user.service.UserService
+import org.slf4j.LoggerFactory
 import org.springframework.http.server.ServerHttpRequest
 import org.springframework.http.server.ServerHttpResponse
 import org.springframework.http.server.ServletServerHttpRequest
@@ -18,30 +19,59 @@ class AuthHandshakeInterceptor(
     private val authService: AuthService,
     private val userService: UserService,
 ) : HandshakeInterceptor {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     override fun beforeHandshake(
         request: ServerHttpRequest,
         response: ServerHttpResponse,
         wsHandler: WebSocketHandler,
         attributes: MutableMap<String, Any>,
     ): Boolean {
+        // NOTE: 핸드셰이킹 과정 중 accessToken 만료에 대한 고민이 필요하다.
         val accessToken = findAccessToken(request) ?: return false
 
-        val userId = try {
-            authService.findUserIdByAccessToken(accessToken).toLong()
-        } catch (_: InvalidCredentialsException) {
-            return false
-        }
+        // NOTE: 핸드셰이킹 이전에 userId와 userNickname을 찾는 것이 맞는가?
+        // NOTE: 인터셉터 분리하면 될 것 같은데
+        val userId = findUserId(accessToken) ?: return false
 
-        val userNickname = try {
-            userService.getUserNickname(userId)
-        } catch (_: UserNotFoundException) {
-            return false
-        }
+        val userNickname = findUserNickname(userId) ?: return false
 
         attributes["userId"] = userId
         attributes["userNickname"] = userNickname
 
         return true
+    }
+
+    override fun afterHandshake(
+        request: ServerHttpRequest,
+        response: ServerHttpResponse,
+        wsHandler: WebSocketHandler,
+        exception: Exception?,
+    ) {
+        val servletRequest = (request as ServletServerHttpRequest).servletRequest
+
+        val remoteAddr = servletRequest.remoteAddr
+        val userAgent = servletRequest.getHeader("User-Agent")
+        val uri = servletRequest.requestURI
+
+        val accessToken = findAccessToken(request) ?: return
+
+        val userId = findUserId(accessToken) ?: return
+
+        val userNickname = findUserNickname(userId) ?: return
+
+        logger.info("""
+            {
+              "event": "WebSocketHandshake",
+              "status": "${if (exception == null) "SUCCESS" else "FAILURE"}",
+              "ip": "$remoteAddr",
+              "uri": "$uri",
+              "userAgent": "$userAgent",
+              "userId": "$userId",
+              "userNickname": "$userNickname"
+            }
+        """.trimIndent()
+        )
     }
 
     private fun findAccessToken(request: ServerHttpRequest): String? {
@@ -55,12 +85,19 @@ class AuthHandshakeInterceptor(
             ?.takeIf { it.isNotBlank() }
     }
 
-    override fun afterHandshake(
-        request: ServerHttpRequest,
-        response: ServerHttpResponse,
-        wsHandler: WebSocketHandler,
-        exception: Exception?,
-    ) {
-        // TODO: 로깅
+    private fun findUserId(accessToken: String): Long? {
+        return try {
+            authService.findUserIdByAccessToken(accessToken).toLong()
+        } catch (_: InvalidCredentialsException) {
+            return null
+        }
+    }
+
+    private fun findUserNickname(userId: Long): String? {
+        return try {
+            userService.getUserNickname(userId)
+        } catch (_: UserNotFoundException) {
+            return null
+        }
     }
 }
